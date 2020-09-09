a8c6a504ad1a52772e2c32f96366f909b60714a5
Joe V <joeveverka89@gmail.com>
2020-09-09 12:27:59 -0400

    added Queue class

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,42 @@
+Class Queue():
+
+	def __init__(self):
+		self.queue = []
+
+	def enqueue(self, value):
+		self.queue.append(value)
+
+	def dequeue(self, value):
+		if self.size() > 0:
+			return self.queue.pop()
+		else: 
+			return None
+
+	def size(self):
+		return len(self.queue)
+
+	def empty(self):
+		return self.size() <= 0
+
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+
+	# graph initialization
+	graph = {}
+
+	# graph will maintain O(n) over ancestors
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+    
